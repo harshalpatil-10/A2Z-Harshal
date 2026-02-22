315cda9ed2438fa1afefec76e3ab4cb6d885c536
Harshal Patil <patilharshal39730@gmail.com>
2026-02-22 17:55:02 +0530

    Implement prime number checking function

---
+++ PrimeNumbers.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPrime(int n) {
+
+        if (n <= 1) {
+            return false;
+        }
+
+        int cnt = 0;
+
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                cnt++;
+
+                if ((n / i) != i) {
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt == 2;
+    }
+}
